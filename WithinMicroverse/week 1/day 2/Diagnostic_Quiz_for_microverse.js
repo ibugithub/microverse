1818dea17017130fb53ee8849404daec6e320bc5
ibugithub <outsideworkibrahim@gmail.com>
2023-03-31 06:52:28 +0600

    Complete the week1(repetead)

---
+++ WithinMicroverse/week 1/day 2/Diagnostic_Quiz_for_microverse.js
@@ -0,0 +1,19 @@
+let foo = 22;
+foo = 10^3 + 10
+let f1 = 0;
+let f2 = 0;
+
+console.log(f1 || f2)
+
+let vegetable = ['mango', 'tango']
+let vegetable2 = Array['carrot', 'parrot']
+console.log(vegetable)
+console.log(vegetable2)
+
+
+
+
+let book = {title :"ibusland", author : "ibu"}
+
+console.log(book (title))
+console.log(vegetable2)
---
+++ WithinMicroverse/week 1/day 2/linter/index.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line 
+let fruit = "mango"
+
+// eslint-disable-next-line
+let did_show_messege = false
+function show_messege() 
+{
+    const messege = " I love to code "
+for (let i = 0; i < 5; i++) 
+{
+    console.log(messege)
+}
+}
+show_messege()
